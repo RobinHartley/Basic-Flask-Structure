62ef4abb76b40a810510c9aa2b88a0e6ac9bad3e
RobinHartley <robinh94@live.co.uk>
2019-05-12 18:54:23 +0100

    Added Waitress web server to start.py

--- start.py
+++ start.py
@@ -5,6 +5,6 @@ app = create_app()
 if __name__ == '__main__':                  # Startup stuff
     app.run(host='127.0.0.1', port=5000)    # Specify the port and url
 
-    # from waitress import serve
-    # serve(app, host='0.0.0.0', port=80)
+    # from waitress import serve                # Waitress is a production server. It can serve multiple simultaneous requests
+    # serve(app, host='0.0.0.0', port=80)       # Serve on the public IP address of the host computer
 
